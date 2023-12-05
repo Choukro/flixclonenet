e57e71ecd660134e3b42efa3d9f5ebea1fff0ec5
Pierre-Henri Merrer <phmerrer@gmail.com>
2023-12-05 10:41:44 +0100

    NetflixApp : general structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+// 'use client'
 
+import {NetflixApp} from '../components/NetflixApp'
+// import {createTheme, ThemeProvider} from '@mui/material/styles'
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#111',
+//     },
+//     secondary: {
+//       main: '#000',
+//     },
+//   },
+// })
 
 export default function Home() {
   return (
-    <div></div>
+  //  <ThemeProvider theme={theme}>
+      <NetflixApp />
+    // </ThemeProvider>
   )
 }
